17ab133775f64b22570b656ae221689b3e71e396
TREVOR69-ops <trevoranastscia@gmail.com>
2025-07-04 16:58:44 +0800

    Add files via upload

---
+++ effects2.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imgSrc = "https://lanyard.cnrad.dev/api/1363905452815028497?bg=00000000&animated=true=true&hideSpotify=true&showDisplayName=true&hideActivity=true";
+    const imgElement = document.getElementById('rpc');
+    imgElement.src = imgSrc;
+});
